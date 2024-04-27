90a4c84776e5fab8bd2aa36b4d69e97417ce0b93
TanmayAdithya <marnitanmayadithya@gmail.com>
2024-04-27 21:18:02 +0530

    logo deployment error fixed AGAIN

---
+++ .vercel/output/functions/_render.func/home/berserker/work/github/astro-blog/.vercel/output/_functions/chunks/404_ByN-CXrx.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/404_CF96-D0t.mjs').then(n => n._);
+
+export { page };
---
+++ .vercel/output/functions/_render.func/home/berserker/work/github/astro-blog/.vercel/output/_functions/chunks/_.._BeJBhfdq.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/__vk4eIKAf.mjs').then(n => n._);
+
+export { page };
