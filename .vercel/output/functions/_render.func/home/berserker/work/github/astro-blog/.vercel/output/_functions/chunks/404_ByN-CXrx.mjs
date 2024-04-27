58a09d6d750b3f3cadf94f01dbeba1fdfc806fdf
TanmayAdithya <marnitanmayadithya@gmail.com>
2024-04-27 21:19:05 +0530

    i hate life

--- .vercel/output/functions/_render.func/home/berserker/work/github/astro-blog/.vercel/output/_functions/chunks/404_ByN-CXrx.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/404_CF96-D0t.mjs').then(n => n._);
-
-export { page };
--- .vercel/output/functions/_render.func/home/berserker/work/github/astro-blog/.vercel/output/_functions/chunks/search_CX6cY4Qb.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/search_C9y2Cgxa.mjs');
-
-export { page };
